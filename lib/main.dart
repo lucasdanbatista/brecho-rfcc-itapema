fe2d75e2fea065f47e911eea2ecd7b02ce93a23b
Lucas Batista <lucas.batista@mob4pay.com.br>
2024-06-12 22:07:35 -0300

    Remove imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
-import 'package:vienna/core/cart_manager.dart';
 
 import 'core/core_module.dart';
 import 'core/module.dart';
